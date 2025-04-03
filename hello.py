3e3dc75d6fadb1f48ebd0e33776beb06397d248d
joao_pena <joao.pena@smarketsolutions.com.br>
2025-04-03 09:07:21 -0300

    Criando o programa Hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
-print("hello world!!!".upper())
-print(58+7)
+
+'''Hello world multi linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a msg 
+correspondente
+
+Como usar: 
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+
+'''
+__version__ = "0.0.1"
+__autor__ = "João Paulo"
+__licence__ = "Unlicense"
+
+import os
+
+#current_language = "en_US"
+current_language = os.getenv("LANG","en_US")[:5]
+msg="Hello, World"
+if current_language == "pt_BR":
+    msg="Olá, Mundo!"
+elif current_language == "it_IT":
+    msg="Ciao, Mondo!"
+
+print(msg)
